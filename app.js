21b4104f180156da583129c1eac3c23f51b5eba2
Noah Giesel <noahgiesel@gmail.com>
2019-05-31 09:53:14 +0200

    landing page ready to use

--- app.js
+++ app.js
@@ -8,13 +8,11 @@ let toggler = false;
 function searchForItem() { 
     if(!toggler) {   
         search.innerHTML = '<i class="fas fa-times"></i>'
-        input.style.transform = "translateY(0%)"
-        search.style.backgroundColor="#333" 
+        input.style.transform = "translateY(0%)" 
         toggler = !toggler; 
      }else { 
         search.innerHTML = '<i class="fas fa-search"></i>' 
-        input.style.transform = "translateY(150%)"
-        search.style.backgroundColor="#f2f2f2" 
+        input.style.transform = "translateY(150%)" 
         toggler = !toggler;
      } 
 }
